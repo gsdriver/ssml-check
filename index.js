1273e399caab940e96800c584ba8b630a5b861b9
Garrett Vargas <gvargas@SEA.CORP.EXPECN.COM>
2019-03-28 15:30:19 -0700

    Check for valid audio files

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 //
 
 const convert = require('xml-js');
+const ffprobe = require('ffprobe');
+const ffprobeStatic = require('ffprobe-static');
 
 function createTagError(element, attribute, undefinedValue) {
   const error = {type: 'tag', tag: element.name};
@@ -56,18 +58,55 @@ function readDuration(text, maximum) {
   return time;
 }
 
-function countAudioFiles(element) {
-  let files = 0;
+function getAudioFiles(element) {
+  let files = [];
 
   if (element.elements) {
     element.elements.forEach((item) => {
-      files += countAudioFiles(item);
+      files = files.concat(getAudioFiles(item));
     });
-  } else if (element.name === 'audio') {
-    files = 1;
+  } else if ((element.name === 'audio') && (element.attributes.src)) {
+    files.push(element.attributes.src);
   }
 
   return files;
+}
+
+function validateAudio(src) {
+  const errors = [];
+
+  // It can be one of the built-in Amazon sounds (from the soundbank)
+
+  // Must be MP3 at HTTPS endpoint
+  if (!src.match(/^https(.)+\.mp3/gi)) {
+    errors.push({type: 'audio', value: src, detail: 'Not MP3 on HTTPS'});
+    return Promise.resolve(errors);
+  }
+
+  // Make sure we can access the audio file
+  // The sample rate must be 22050Hz, 24000Hz, or 16000Hz
+  // and the bit rate must be 48kbps
+  // audio file length cannot be more than 240 seconds
+  return ffprobe(src, {path: ffprobeStatic.path})
+  .then((info) => {
+    info.streams.forEach((stream) => {
+      if ([22050, 24000, 16000].indexOf(parseInt(stream.sample_rate)) === -1) {
+        errors.push({type: 'audio', value: src, detail: `Invalid bit rate ${stream.sample_rate} Hz`})
+      }
+      if (stream.bit_rate != 48000) {
+        errors.push({type: 'audio', value: src, detail: `Invalid bit rate ${stream.bit_rate}`})
+      }
+      if (stream.duration > 240) {
+        errors.push({type: 'audio', value: src, detail: `Invalid duration ${stream.duration} Hz`})
+      }
+    });
+
+    return errors;
+  }).catch((err) => {
+    // We can't read this audio file
+    errors.push({type: 'audio', value: src, detail: 'Can\'t access file'});
+    return errors;
+  });
 }
 
 function checkForValidTags(errors, element, platform) {
@@ -415,17 +454,17 @@ function checkForValidTags(errors, element, platform) {
 
 module.exports = {
   check: function(ssml, options) {
-    const errors = [];
+    let errors = [];
 
     try {
       let result;
       let text = ssml;
-      const userOptions = options || {platform: 'all'};
-      userOptions.checkVUI = userOptions.checkVUI || {};
+      const userOptions = options || {};
+      userOptions.platform = userOptions.platform || 'all';
 
       if (['all', 'amazon', 'google'].indexOf(userOptions.platform) === -1) {
         errors.push({type: 'invalid platform'});
-        return errors;
+        return Promise.resolve(errors);
       }
 
       // This needs to be a single item wrapped in a speak tag
@@ -437,7 +476,7 @@ module.exports = {
           speech = result.elements[0];
         } else {
           errors.push({type: 'tag', tag: 'speak'});
-          return errors;
+          return Promise.resolve(errors);
         }
       } catch (err) {
         // Special case - if we replace & with &amp; does it fix it?
@@ -451,16 +490,32 @@ module.exports = {
           // Nope, it's some other error
           errors.push({type: 'Can\'t parse SSML'});
         }
-        return errors;
+        return Promise.resolve(errors);
       }
 
       // Make sure only valid tags are present
       checkForValidTags(errors, speech, userOptions.platform);
 
       // Count the audio files - is it more than 5?
-      const audio = countAudioFiles(speech);
-      if (audio > 5) {
+      const audio = getAudioFiles(speech);
+      if (audio.length > 5) {
         errors.push({type: 'Too many audio files'});
+      }
+
+      // If they asked to validate audio files, do that now
+      if (userOptions.validateAudioFiles) {
+        const promises = [];
+
+        audio.forEach((file) => {
+          promises.push(validateAudio(file));
+        });
+
+        return Promise.all(promises).then((audioErrors) => {
+          audioErrors.forEach((audioError) => {
+            errors = errors.concat(audioError);
+          });
+          return (errors.length ? errors : undefined);
+        });
       }
     } catch (err) {
       console.log(err);
@@ -468,6 +523,6 @@ module.exports = {
     }
 
     // OK, looks like it's OK!
-    return (errors.length ? errors : undefined);
-  }
+    return Promise.resolve(errors.length ? errors : undefined);
+  },
 };
--- test/test.js
+++ test/test.js
@@ -5,110 +5,124 @@ let succeeded = 0;
 let failed = 0;
 
 function runTest(testName, ssml, options, expectedResult) {
-  const retVal = lib.check(ssml, options);
-  let result;
+  return lib.check(ssml, options).then((retVal) => {
+    let result;
 
-  if (retVal) {
-    result = '';
-    retVal.forEach((value) => {
-      if (value.tag) {
-        result += value.tag + ' tag has invalid ';
-        if (value.value) {
-          result += value.attribute + ' value ' + value.value;
-        } else if (value.attribute) {
-          result += 'attribute ' + value.attribute;
+    if (retVal) {
+      result = '';
+      retVal.forEach((value) => {
+        if (value.type === 'audio') {
+          // It's an audio error
+          result += 'audio file ' + value.value + ' ' + value.detail;
+        } else if (value.tag) {
+          result += value.tag + ' tag has invalid ';
+          if (value.value) {
+            result += value.attribute + ' value ' + value.value;
+          } else if (value.attribute) {
+            result += 'attribute ' + value.attribute;
+          }
+        } else {
+          result = value.type;
         }
-      } else {
-        result = value.type;
-      }
-    });
-  } else {
-    result = 'valid';
-  }
+      });
+    } else {
+      result = 'valid';
+    }
 
-  if (result == expectedResult) {
-    succeeded++;
-  } else {
-    console.log('FAIL: ' + testName + ' returned ' + result + ' rather than ' + expectedResult);
-    failed++;
-  }
+    if (result == expectedResult) {
+      succeeded++;
+    } else {
+      console.log('FAIL: ' + testName + ' returned ' + result + ' rather than ' + expectedResult);
+      failed++;
+    }
+
+    return 0;
+  });
 }
 
 const start = Date.now();
+const promises = [];
 
-runTest('Simple SSML', '<speak>Simple test</speak>', null, 'valid');
+promises.push(runTest('Simple SSML', '<speak>Simple test</speak>', null, 'valid'));
 
 // Whisper tests
-runTest('Whisper effect', '<speak><amazon:effect name="whispered">Simple test <break strength="medium"/> code</amazon:effect></speak>', {platform: 'amazon'}, 'valid');
-runTest('Whisper effect', '<speak><amazon:effect name="whispering">Simple test <break strength="medium"/> code</amazon:effect></speak>', {platform: 'amazon'}, 'amazon:effect tag has invalid name value whispering');
+promises.push(runTest('Whisper effect', '<speak><amazon:effect name="whispered">Simple test <break strength="medium"/> code</amazon:effect></speak>', {platform: 'amazon'}, 'valid'));
+promises.push(runTest('Whisper effect', '<speak><amazon:effect name="whispering">Simple test <break strength="medium"/> code</amazon:effect></speak>', {platform: 'amazon'}, 'amazon:effect tag has invalid name value whispering'));
 
 // Audio tests
-runTest('Valid audio', '<speak><audio src="foo.mp3" clipBegin="2.2s" clipEnd="3000ms" repeatCount="3"/> You like that?</speak>', {platform: 'google'}, 'valid');
-runTest('Invalid Amazon audio', '<speak><audio src="foo.mp3" clipBegin="2.2s" clipEnd="3000ms" repeatCount="3"/> You like that?</speak>', {platform: 'amazon'}, 'audio tag has invalid attribute clipBeginaudio tag has invalid attribute clipEndaudio tag has invalid attribute repeatCount');
+promises.push(runTest('Valid audio', '<speak><audio src="foo.mp3" clipBegin="2.2s" clipEnd="3000ms" repeatCount="3"/> You like that?</speak>', {platform: 'google'}, 'valid'));
+promises.push(runTest('Invalid Amazon audio', '<speak><audio src="foo.mp3" clipBegin="2.2s" clipEnd="3000ms" repeatCount="3"/> You like that?</speak>', {platform: 'amazon'}, 'audio tag has invalid attribute clipBeginaudio tag has invalid attribute clipEndaudio tag has invalid attribute repeatCount'));
 
 // Break tests
-runTest('With break', '<speak>You lost <break time="200ms"/> Getting used to losing?  Take a break and come back tomorrow</speak>', null, 'valid');
-runTest('With break in seconds', '<speak>You lost <break time="2.5s"/> Getting used to losing?  Take a break and come back tomorrow</speak>', null, 'valid');
-runTest('Break with bad attribute', '<speak>You lost <break tim="200ms"/> Getting used to losing?  Take a break and come back tomorrow</speak>', null, 'break tag has invalid attribute tim');
-runTest('Break with long attribute', '<speak>You lost <break time="200s"/> Getting used to losing?  Take a break and come back tomorrow</speak>', null, 'break tag has invalid time value 200s');
+promises.push(runTest('With break', '<speak>You lost <break time="200ms"/> Getting used to losing?  Take a break and come back tomorrow</speak>', null, 'valid'));
+promises.push(runTest('With break in seconds', '<speak>You lost <break time="2.5s"/> Getting used to losing?  Take a break and come back tomorrow</speak>', null, 'valid'));
+promises.push(runTest('Break with bad attribute', '<speak>You lost <break tim="200ms"/> Getting used to losing?  Take a break and come back tomorrow</speak>', null, 'break tag has invalid attribute tim'));
+promises.push(runTest('Break with long attribute', '<speak>You lost <break time="200s"/> Getting used to losing?  Take a break and come back tomorrow</speak>', null, 'break tag has invalid time value 200s'));
 
 // Emphasis tests
-runTest('Valid emphasis', '<speak>I already told you I <emphasis level="strong">really like</emphasis> that person. </speak>', null, 'valid');
-runTest('Bad emphasis', '<speak>I already told you I <emphasis level="cute">really like</emphasis> that person. </speak>', null, 'emphasis tag has invalid level value cute');
-runTest('Google emphasis', '<speak>I already told you I <emphasis level="none">really like</emphasis> that person. </speak>', {platform: 'google'}, 'valid');
+promises.push(runTest('Valid emphasis', '<speak>I already told you I <emphasis level="strong">really like</emphasis> that person. </speak>', null, 'valid'));
+promises.push(runTest('Bad emphasis', '<speak>I already told you I <emphasis level="cute">really like</emphasis> that person. </speak>', null, 'emphasis tag has invalid level value cute'));
+promises.push(runTest('Google emphasis', '<speak>I already told you I <emphasis level="none">really like</emphasis> that person. </speak>', {platform: 'google'}, 'valid'));
 
 // Lang tests
-runTest('Valid lang', '<speak><lang xml:lang="fr-FR">J\'adore chanter</lang></speak>', {platform: 'amazon'}, 'valid');
-runTest('Invalid lang', '<speak><lang xml:lang="pt-BR">Blame it on Rior</lang></speak>', {platform: 'amazon'}, 'lang tag has invalid xml:lang value pt-BR');
+promises.push(runTest('Valid lang', '<speak><lang xml:lang="fr-FR">J\'adore chanter</lang></speak>', {platform: 'amazon'}, 'valid'));
+promises.push(runTest('Invalid lang', '<speak><lang xml:lang="pt-BR">Blame it on Rior</lang></speak>', {platform: 'amazon'}, 'lang tag has invalid xml:lang value pt-BR'));
 
 // p tests
-runTest('Valid p', '<speak><p>This is the first paragraph. There should be a pause after this text is spoken.</p><p>This is the second paragraph.</p></speak>', null, 'valid');
-runTest('Invalid p', '<speak><p dog="cute">This is the first paragraph. There should be a pause after this text is spoken.</p><p>This is the second paragraph.</p></speak>', null, 'p tag has invalid attribute dog');
+promises.push(runTest('Valid p', '<speak><p>This is the first paragraph. There should be a pause after this text is spoken.</p><p>This is the second paragraph.</p></speak>', null, 'valid'));
+promises.push(runTest('Invalid p', '<speak><p dog="cute">This is the first paragraph. There should be a pause after this text is spoken.</p><p>This is the second paragraph.</p></speak>', null, 'p tag has invalid attribute dog'));
 
 // phoneme tests
-runTest('Valid phoneme', '<speak>You say, <phoneme alphabet="ipa" ph="pɪˈkɑːn">pecan</phoneme>. I say, <phoneme alphabet="ipa" ph="ˈpi.kæn">pecan</phoneme>.</speak>', {platform: 'amazon'}, 'valid');
-runTest('Invalid phoneme', '<speak>You say, <phoneme alphabet="ipa2">pecan</phoneme>. I say, <phoneme alphabet="ipa" ph="ˈpi.kæn">pecan</phoneme>.</speak>', {platform: 'amazon'}, 'phoneme tag has invalid alphabet value ipa2');
+promises.push(runTest('Valid phoneme', '<speak>You say, <phoneme alphabet="ipa" ph="pɪˈkɑːn">pecan</phoneme>. I say, <phoneme alphabet="ipa" ph="ˈpi.kæn">pecan</phoneme>.</speak>', {platform: 'amazon'}, 'valid'));
+promises.push(runTest('Invalid phoneme', '<speak>You say, <phoneme alphabet="ipa2">pecan</phoneme>. I say, <phoneme alphabet="ipa" ph="ˈpi.kæn">pecan</phoneme>.</speak>', {platform: 'amazon'}, 'phoneme tag has invalid alphabet value ipa2'));
 
 // prosody tests
-runTest('Valid rate', '<speak><prosody rate="slow">Hello world</prosody></speak>', null, 'valid');
-runTest('Invalid rate', '<speak><prosody rate="xx-large">Hello world</prosody></speak>', null, 'prosody tag has invalid rate value xx-large');
-runTest('Valid rate percent', '<speak><prosody rate="150%">Hello world</prosody></speak>', null, 'valid');
-runTest('Invalid rate percent', '<speak><prosody rate="5%">Hello world</prosody></speak>', null, 'prosody tag has invalid rate value 5%');
-runTest('Valid pitch', '<speak><prosody pitch="low">Hello world</prosody></speak>', null, 'valid');
-runTest('Invalid pitch', '<speak><prosody pitch="x-small">Hello world</prosody></speak>', null, 'prosody tag has invalid pitch value x-small');
-runTest('Valid pitch percent positive', '<speak><prosody pitch="+20%">Hello world</prosody></speak>', null, 'valid');
-runTest('Valid pitch percent negative', '<speak><prosody pitch="-10.5%">Hello world</prosody></speak>', null, 'valid');
-runTest('Invalid pitch percent positive', '<speak><prosody pitch="+60%">Hello world</prosody></speak>', null, 'prosody tag has invalid pitch value +60%');
-runTest('Invalid pitch percent negative', '<speak><prosody pitch="-40%">Hello world</prosody></speak>', null, 'prosody tag has invalid pitch value -40%');
-runTest('Invalid pitch percent format', '<speak><prosody pitch="+2.5.6%">Hello world</prosody></speak>', null, 'prosody tag has invalid pitch value +2.5.6%');
-runTest('Valid volume', '<speak><prosody volume="loud">Hello world</prosody></speak>', null, 'valid');
-runTest('Invalid volume', '<speak><prosody volume="xx-large">Hello world</prosody></speak>', null, 'prosody tag has invalid volume value xx-large');
-runTest('Valid volume dB', '<speak><prosody volume="+4.5dB">Hello world</prosody></speak>', null, 'valid');
-runTest('Invalid volume dB', '<speak><prosody volume="-5.5.5dB">Hello world</prosody></speak>', null, 'prosody tag has invalid volume value -5.5.5dB');
+promises.push(runTest('Valid rate', '<speak><prosody rate="slow">Hello world</prosody></speak>', null, 'valid'));
+promises.push(runTest('Invalid rate', '<speak><prosody rate="xx-large">Hello world</prosody></speak>', null, 'prosody tag has invalid rate value xx-large'));
+promises.push(runTest('Valid rate percent', '<speak><prosody rate="150%">Hello world</prosody></speak>', null, 'valid'));
+promises.push(runTest('Invalid rate percent', '<speak><prosody rate="5%">Hello world</prosody></speak>', null, 'prosody tag has invalid rate value 5%'));
+promises.push(runTest('Valid pitch', '<speak><prosody pitch="low">Hello world</prosody></speak>', null, 'valid'));
+promises.push(runTest('Invalid pitch', '<speak><prosody pitch="x-small">Hello world</prosody></speak>', null, 'prosody tag has invalid pitch value x-small'));
+promises.push(runTest('Valid pitch percent positive', '<speak><prosody pitch="+20%">Hello world</prosody></speak>', null, 'valid'));
+promises.push(runTest('Valid pitch percent negative', '<speak><prosody pitch="-10.5%">Hello world</prosody></speak>', null, 'valid'));
+promises.push(runTest('Invalid pitch percent positive', '<speak><prosody pitch="+60%">Hello world</prosody></speak>', null, 'prosody tag has invalid pitch value +60%'));
+promises.push(runTest('Invalid pitch percent negative', '<speak><prosody pitch="-40%">Hello world</prosody></speak>', null, 'prosody tag has invalid pitch value -40%'));
+promises.push(runTest('Invalid pitch percent format', '<speak><prosody pitch="+2.5.6%">Hello world</prosody></speak>', null, 'prosody tag has invalid pitch value +2.5.6%'));
+promises.push(runTest('Valid volume', '<speak><prosody volume="loud">Hello world</prosody></speak>', null, 'valid'));
+promises.push(runTest('Invalid volume', '<speak><prosody volume="xx-large">Hello world</prosody></speak>', null, 'prosody tag has invalid volume value xx-large'));
+promises.push(runTest('Valid volume dB', '<speak><prosody volume="+4.5dB">Hello world</prosody></speak>', null, 'valid'));
+promises.push(runTest('Invalid volume dB', '<speak><prosody volume="-5.5.5dB">Hello world</prosody></speak>', null, 'prosody tag has invalid volume value -5.5.5dB'));
 
 // say-as tests
-runTest('Valid say-as Amazon', '<speak><say-as interpret-as="interjection">Wow</say-as></speak>', {platform: 'amazon'}, 'valid');
-runTest('Valid say-as Google', '<speak><say-as interpret-as="bleep">Wow</say-as></speak>', {platform: 'google'}, 'valid');
-runTest('Invalid say-as all', '<speak><say-as interpret-as="bleep">Wow</say-as></speak>', {platform: 'all'}, 'say-as tag has invalid interpret-as value bleep');
-runTest('Valid say-as date', '<speak><say-as interpret-as="date" format="mdy">September 22</say-as></speak>', null, 'valid');
-runTest('Invalid say-as', '<speak><say-as interpret-as="interjections">Wow</say-as></speak>', null, 'say-as tag has invalid interpret-as value interjections');
-runTest('Invalid say-as date', '<speak><say-as interpret-as="date" format="mddy">September 22</say-as></speak>', null, 'say-as tag has invalid format value mddy');
+promises.push(runTest('Valid say-as Amazon', '<speak><say-as interpret-as="interjection">Wow</say-as></speak>', {platform: 'amazon'}, 'valid'));
+promises.push(runTest('Valid say-as Google', '<speak><say-as interpret-as="bleep">Wow</say-as></speak>', {platform: 'google'}, 'valid'));
+promises.push(runTest('Invalid say-as all', '<speak><say-as interpret-as="bleep">Wow</say-as></speak>', {platform: 'all'}, 'say-as tag has invalid interpret-as value bleep'));
+promises.push(runTest('Valid say-as date', '<speak><say-as interpret-as="date" format="mdy">September 22</say-as></speak>', null, 'valid'));
+promises.push(runTest('Invalid say-as', '<speak><say-as interpret-as="interjections">Wow</say-as></speak>', null, 'say-as tag has invalid interpret-as value interjections'));
+promises.push(runTest('Invalid say-as date', '<speak><say-as interpret-as="date" format="mddy">September 22</say-as></speak>', null, 'say-as tag has invalid format value mddy'));
 
 // voice tests
-runTest('Valid voice', '<speak>I want to tell you a secret. <voice name="Kendra">I am not a real human.</voice>. Can you believe it?</speak>', {platform: 'amazon'}, 'valid');
-runTest('Valid voice', '<speak>I want to tell you a secret. <voice name="Samantha">I am not a real human.</voice>. Can you believe it?</speak>', {platform: 'amazon'}, 'voice tag has invalid name value Samantha');
+promises.push(runTest('Valid voice', '<speak>I want to tell you a secret. <voice name="Kendra">I am not a real human.</voice>. Can you believe it?</speak>', {platform: 'amazon'}, 'valid'));
+promises.push(runTest('Valid voice', '<speak>I want to tell you a secret. <voice name="Samantha">I am not a real human.</voice>. Can you believe it?</speak>', {platform: 'amazon'}, 'voice tag has invalid name value Samantha'));
 
 // Media tests
-runTest('Valid media', '<speak><seq><media begin="0.5s"><speak>Who invented the Internet?</speak></media><media begin="2.0s"><speak>The Internet was invented by cats.</speak></media><media soundLevel="-6dB"><audio src="https://actions.google.com/.../cartoon_boing.ogg"/></media><media repeatCount="3" soundLevel="+2.28dB" fadeInDur="2s" fadeOutDur="0.2s"><audio src="https://actions.google.com/.../cat_purr_close.ogg"/></media></seq> </speak>', {platform: 'google'}, 'valid');
+promises.push(runTest('Valid media', '<speak><seq><media begin="0.5s"><speak>Who invented the Internet?</speak></media><media begin="2.0s"><speak>The Internet was invented by cats.</speak></media><media soundLevel="-6dB"><audio src="https://actions.google.com/.../cartoon_boing.ogg"/></media><media repeatCount="3" soundLevel="+2.28dB" fadeInDur="2s" fadeOutDur="0.2s"><audio src="https://actions.google.com/.../cat_purr_close.ogg"/></media></seq> </speak>', {platform: 'google'}, 'valid'));
+
+// Audio validation tests
+promises.push(runTest('Verify valid audio', '<speak><audio src="https://s3-us-west-2.amazonaws.com/alexasoundclips/casinowelcome.mp3"/> You like that?</speak>', {validateAudioFiles: true}, 'valid'));
+promises.push(runTest('Verify invalid audio', '<speak><audio src="https://foo.mp3"/> You like that?</speak>', {validateAudioFiles: true}, 'audio file https://foo.mp3 Can\'t access file'));
+promises.push(runTest('Verify invalid file', '<speak><audio src="https://foo.wav"/> You like that?</speak>', {validateAudioFiles: true}, 'audio file https://foo.wav Not MP3 on HTTPS'));
 
 // Multiple errors
-runTest('Bad break and invalid prosody rate', '<speak>You lost <break tim="200ms"/> Getting used to losing?  <prosody rate="xx-large">Take a break and come back tomorrow</prosody></speak>', null, 'break tag has invalid attribute timprosody tag has invalid rate value xx-large');
+promises.push(runTest('Bad break and invalid prosody rate', '<speak>You lost <break tim="200ms"/> Getting used to losing?  <prosody rate="xx-large">Take a break and come back tomorrow</prosody></speak>', null, 'break tag has invalid attribute timprosody tag has invalid rate value xx-large'));
 
 // Invalid formats
-runTest('Invalid XML', '<tag>What is this?', null, 'Can\'t parse SSML');
-runTest('Too many audio files', '<speak><audio src=\"https://www.foo.com/foo.mp3\"/> one <audio src=\"https://www.foo.com/foo.mp3\"/> two <audio src=\"https://www.foo.com/foo.mp3\"/> three <audio src=\"https://www.foo.com/foo.mp3\"/> four <audio src=\"https://www.foo.com/foo.mp3\"/> five <audio src=\"https://www.foo.com/foo.mp3\"/> six </speak>', null, 'Too many audio files');
-runTest('Invalid platform', '<speak>Hello there</speak>', {platform: 'siri'}, 'invalid platform');
-runTest('Invalid ampersand', '<speak>This & that</speak>', null, 'Invalid & character');
+promises.push(runTest('Invalid XML', '<tag>What is this?', null, 'Can\'t parse SSML'));
+promises.push(runTest('Too many audio files', '<speak><audio src=\"https://www.foo.com/foo.mp3\"/> one <audio src=\"https://www.foo.com/foo.mp3\"/> two <audio src=\"https://www.foo.com/foo.mp3\"/> three <audio src=\"https://www.foo.com/foo.mp3\"/> four <audio src=\"https://www.foo.com/foo.mp3\"/> five <audio src=\"https://www.foo.com/foo.mp3\"/> six </speak>', null, 'Too many audio files'));
+promises.push(runTest('Invalid platform', '<speak>Hello there</speak>', {platform: 'siri'}, 'invalid platform'));
+promises.push(runTest('Invalid ampersand', '<speak>This & that</speak>', null, 'Invalid & character'));
 
 // Final summary
-console.log('\r\nRan ' + (succeeded + failed) + ' tests in ' + (Date.now() - start) + 'ms; ' + succeeded + ' passed and ' + failed + ' failed');
+Promise.all(promises).then(() => {
+  console.log('\r\nRan ' + (succeeded + failed) + ' tests in ' + (Date.now() - start) + 'ms; ' + succeeded + ' passed and ' + failed + ' failed');
+});
