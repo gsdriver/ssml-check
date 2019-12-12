34bf761666417d54ce63e899876b0f1b96918652
Andrés Reyes Monge <armonge@gmail.com>
2019-12-12 09:50:44 +0100

    New Typescript declarations file

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+export interface ISSMLCheckOptions {
+  platform?: "google" | "amazon" | "all";
+  validateAudioFiles?: boolean;
+}
+
+export interface ISSMLCheckError {
+  type: string;
+  tag: string;
+  attribute: string;
+  value: string;
+  detail: string;
+}
+
+export interface ISSMLCheckVerifyResponse {
+  errors?: ISSMLCheckError[];
+  ssml?: string;
+}
+
+export function check(
+  ssml: string,
+  options?: ISSMLCheckOptions
+): Promise<ISSMLCheckError[] | undefined>;
+
+export function verifyAndFix(
+  ssml: string,
+  options?: ISSMLCheckOptions
+): Promise<ISSMLCheckVerifyResponse>;
